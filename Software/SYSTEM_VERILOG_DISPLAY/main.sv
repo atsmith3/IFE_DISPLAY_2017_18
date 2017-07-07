7209b729a0d4e34aa0f9fa5fb0c7082fc51b6b77
atsmith3 <atsmith3@illinois.edu>
2017-07-06 21:56:04 -0500

    base FPGA project added

--- Software/SYSTEM_VERILOG_DISPLAY/main.sv
+++ Software/SYSTEM_VERILOG_DISPLAY/main.sv
@@ -1,8 +1,14 @@
-module top(	input logic [7:0]port_in,
+module top(	/* Micro Input */
+				input logic [7:0]port_in,
 				input logic [2:0]addr, 
-				input logic write, Clk,
-				output logic status,		
-				output logic [7:0]red, green, blue);
+				input logic write,
+				/* General Input */
+				input logic Clk, Reset,
+				/* General Output */
+				output logic status,
+				/* Display Output */
+				output logic [7:0]RD, GD, BD,
+				output logic DEN, VSYNC, HSYNC, DISP_CLK, DISP_EN);
 	
 	registers r(.addr(addr),
 					.write(write),
@@ -15,8 +21,6 @@ module top(	input logic [7:0]port_in,
 					.gps_status(),
 					.err_code());
 					
-	background b(.disp_x(), .disp_y(),
-					.color(),
-					.draw());
+	
 	
 endmodule
