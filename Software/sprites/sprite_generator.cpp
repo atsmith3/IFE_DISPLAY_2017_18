4288bd0edd477b2a41546ce863341a9724efb2cc
atsmith3 <atsmith3@illinois.edu>
2017-12-11 19:31:02 -0600

    Trying new scalable sprite generator

--- Software/sprites/sprite_generator.cpp
+++ Software/sprites/sprite_generator.cpp
@@ -5,20 +5,43 @@
 #include "png.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define PALATE_SIZE 8
+#define PALATE_SIZE 3
 
-RGBAPixel ife_blue(17,32,51);
+typedef struct color {
+    int red;
+    int green;
+    int blue;
+} color_t;
+
+color_t palate[PALATE_SIZE] = {{17, 32, 51}, {253,97,12}, {255,255,255}};
 
 int main(void) {
     PNG image("startupscreenlogo.png");
+    double distance = 0.0;
+    double min_distance = 0.0;
+    int color = 0;
 
     printf("'{");
     for(size_t i = 0; i < image.height(); i++) {
         printf("'{");
         for(size_t j = 0; j < image.width(); j++) {
-            if(image(j,i)->blue == ife_blue.blue) printf("0");
-            else printf("1");
+            min_distance = sqrt(3*pow(255, 2));
+            color = 0;
+            for(int k = 0; k < PALATE_SIZE; k++) {
+                /* Determine the closest approximate color */
+                distance = pow((image(j,i)->blue - palate[k].blue), 2.0) +
+                           pow((image(j,i)->green - palate[k].green), 2.0) +
+                           pow((image(j,i)->red - palate[k].red), 2.0);
+                distance = sqrt(distance);
+                if(distance < min_distance) {
+                    /* Update distance and color */
+                    color = k;
+                    min_distance = distance;
+                }
+            }
+            printf("%2d", color);
             if(j != image.width() - 1) printf(",");
         }
         printf("}");
